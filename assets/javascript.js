71b87941d01c83664f9276b6356bd979d6b756c3
Laura Bullek <74018395+Laura-Bullek@users.noreply.github.com>
2021-02-04 19:09:24 -0500

    Linked elements to the DOM

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,5 @@
- 
+ // Linked elements to the DOM
+const homePageEl = document.getElementById("homePage");
+const questionsPageEl = document.getElementById("questionsPage");
+const gameOverPageEl = document.getElementById("gameOverPage");
+const scoresPageEl = document.getElementById("scoresPage");
